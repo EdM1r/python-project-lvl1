7c83a03709a1108a5d271b6451d7604db4855423
EdMir <canonnist@gmail.com>
2022-01-04 20:58:27 +0100

    adding greeting functionality

---
+++ brain_games/cli.py
@@ -0,0 +1,13 @@
+import prompt 
+
+def welcome_user():
+  name = prompt.string('May I have your name? ')
+  print(f'Hello, {name}!')
+
+
+def main():
+  print('test')
+  welcome_user()
+
+if __name__ == '__main__':
+  main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 #test project for hexlet lvl1 
+from brain_games.cli import welcome_user
 
 def main():
   print('Welcome to Brain Games!')
+  welcome_user()
 
 if __name__ == '__main__':
   main()

a90809f4c7ebcd71018c6d2dc30d14d82e5eb18b
EdMir <canonnist@gmail.com>
2022-01-06 14:32:08 +0100

    set up linter and fixed linter errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,15 @@
-import prompt 
+import prompt
+
 
 def welcome_user():
-  name = prompt.string('May I have your name? ')
-  print(f'Hello, {name}!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
 
 def main():
-  print('test')
-  welcome_user()
+    print('test')
+    welcome_user()
+
 
 if __name__ == '__main__':
-  main()
+    main()

4344e560e92e838aad43e5ba7d1bc1f7360effc0
EdMir <canonnist@gmail.com>
2022-01-11 18:53:58 +0100

    fixed typo in game engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,7 +10,7 @@ def engine(generate_expression, rules):
     while iteration < ITTERATION_MAX:
         expression, correct_answer = generate_expression()
         correct_answer = str(correct_answer)
-        print(f'Questions: {expression}')
+        print(f'Question: {expression}')
         player_input = input("Your answer: ")
         if correct_answer == player_input:
             print('Correct!')
